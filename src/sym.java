5cbc7eca867ffaa7e220f80ad869a2a522a33bc6
Eymardes <eymardesveras@outlook.com>
2025-06-06 01:10:51 -0300

    add projeto

---
+++ src/sym.java
@@ -0,0 +1,29 @@
+public class sym {
+    public static final int IF = 1;
+    public static final int ELSE = 2;
+    public static final int WHILE = 3;
+    public static final int RETURN = 4;
+
+    public static final int IGUAL = 5;
+    public static final int DIFERENTE = 6;
+    public static final int MAIOR = 7;
+    public static final int MENOR = 8;
+    public static final int MAIOR_IGUAL = 9;
+    public static final int MENOR_IGUAL = 10;
+    public static final int ATRIBUI = 11;
+    public static final int MAIS = 12;
+    public static final int MENOS = 13;
+    public static final int MULT = 14;
+    public static final int DIV = 15;
+
+    public static final int PV = 16;
+    public static final int VIRG = 17;
+    public static final int AP = 18;
+    public static final int FP = 19;
+    public static final int AC = 20;
+    public static final int FC = 21;
+
+    public static final int NUM_INT = 22;
+    public static final int NUM_FLOAT = 23;
+    public static final int ID = 24;
+}
